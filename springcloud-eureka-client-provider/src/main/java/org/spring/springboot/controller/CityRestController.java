7f12a8d48c7d9c182e05547e72d836a18fb1e03f
harry.zhang <zjf2671@126.com>
2017-08-04 12:33:33 +0800

    格式化代码

--- springcloud-eureka-client-provider/src/main/java/org/spring/springboot/controller/CityRestController.java
+++ springcloud-eureka-client-provider/src/main/java/org/spring/springboot/controller/CityRestController.java
@@ -2,17 +2,13 @@ package org.spring.springboot.controller;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.spring.springboot.domain.City;
 import org.spring.springboot.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
